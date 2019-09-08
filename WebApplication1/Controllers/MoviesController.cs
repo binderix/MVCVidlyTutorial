c58e8b254998bae87f9b7beac8d13d09d2f2897a
binderix <bg.binder@gmx.de>
2019-09-08 21:44:38 +0200

    second commit

---
+++ WebApplication1/Controllers/MoviesController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.ViewModels;
+using WebApplication1.ViewModels2;
+using WebApplication1.Models;
+
+
+
+namespace WebApplication1.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek" };
+
+            //ViewData["Movie"] = movie;
+            //ViewBag.Movie = movie;
+
+
+            ///////////////////////
+            ///var movie = new Movie() { Name = "Shrek" };
+            var customers = new List<Customer>
+            {
+                new Customer {Name="Harry Heinzel"},
+                new Customer {Name="Nadja Nickel"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+        // GET: Movies/Vidly
+        public ActionResult Vidly()
+        {
+            List<Movie> movies = new List<Movie> {
+                new Movie() { Name = "Schneewittchen" },
+                new Movie() { Name = "Wall-e"}
+            };
+
+            var movieList = new ViewModels.MovieList
+            {
+                movies = movies
+            };
+
+
+            return View(movieList);
+            //return Content("Hello World");
+        }
+
+        // GET: Movies/Customer
+        public ActionResult Customer()
+        {
+            var movie = new Movie() { Name = "Shrek" };
+            var customers = new List<Customer>
+            {
+                new Customer {Name="Harry Heinzel"},
+                new Customer {Name="Nadja Nickel"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ WebApplication1/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.ViewModels;
+using WebApplication1.Models;
+
+namespace WebApplication1.Models
+{
+    public class Customer
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+    }
+}
